5c7b02a9b09945975a4e6f45ef78215f99e32668
Laura Roscelle <93408918+Laura-rosce@users.noreply.github.com>
2024-03-17 20:49:38 -0300

    Add files via upload

---
+++ funções.js
@@ -0,0 +1,22 @@
+function addProduto(){
+    let produtos = []
+
+    
+}
+
+function buscarProduto(){
+
+}
+
+function atualizarProduto(){
+
+}
+
+function visualizarProduto(){
+
+}
+
+
+function removeProduto(){
+
+}
